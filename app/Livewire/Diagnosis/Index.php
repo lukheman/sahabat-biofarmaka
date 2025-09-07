7153bba23e27e30b83f5d600758e7349e3224ef2
Akmal <akmalthemalong@gmail.com>
2025-09-07 13:48:56 +0800

    feat: hanya tampilkan gejala dari penyakit tertent, jangan semua gejala

--- app/Livewire/Diagnosis/Index.php
+++ app/Livewire/Diagnosis/Index.php
@@ -3,6 +3,8 @@
 namespace App\Livewire\Diagnosis;
 
 use App\Models\Gejala;
+use App\Models\GejalaPenyakitTanaman;
+use App\Models\PenyakitTanaman;
 use App\Models\Tanaman;
 use App\Providers\CertaintyFactorProvider;
 use Livewire\Attributes\Computed;
@@ -26,26 +28,38 @@ class Index extends Component
 
     public $showHasil = false;
 
+    public $gejalaList = [];
+
     #[Computed]
     public function tanaman() {
         return Tanaman::all();
     }
 
-    #[Computed]
-    public function gejala()
-    {
-        if ($this->selectedTanaman) {
-            return Gejala::query()->get();
-        }
-        return collect(); // Return empty collection until a plant is selected
-    }
-
     public function selectPlant(int $id)
     {
-        $this->selectedTanaman = Tanaman::query()->find($id);
+        $this->selectedTanaman = Tanaman::query()->with(['penyakit'])->find($id);
         $this->currentGejala = Gejala::query()->find($id);
         $this->certaintyFactorUser = []; // Reset previous answers
         $this->showHasil = false; // Ensure result is hidden
+
+        $penyakitIds = $this->selectedTanaman->penyakit->pluck('id')->toArray();
+
+        $this->gejalaList = GejalaPenyakitTanaman::query()
+            ->whereIn('id_penyakit_tanaman', $penyakitIds)
+            ->distinct('id_gejala')
+            ->pluck('id_gejala')
+            ->pipe(function ($gejalaIds) {
+                return Gejala::query()->whereIn('id', $gejalaIds)->get();
+            });
+
+        // set gejala pertama
+        $this->currentGejala = $this->gejalaList->first();
+
+            // ->pipe(function ($gejala) {
+            //     $this->currentGejala = $gejala->first();
+            //     return $gejala;
+            // });
+
     }
 
     public function setCeraintyFactor()
@@ -67,12 +81,21 @@ class Index extends Component
         $this->setCeraintyFactor();
         $this->reset('currentCeraintyFactor');
 
-        $gejala = Gejala::query()->where('id', '>', $this->currentGejala->id)->first();
+        if(!$this->currentGejala){
+            return;
+        }
 
-        if ($gejala) {
-            $this->currentGejala = $gejala;
+
+        // cari index gejala saat ini
+        $currentIndex = $this->gejalaList->search(fn($g) => $g->id === $this->currentGejala->id);
+
+        // ambil gejala berikutnya
+        $nextGejala = $this->gejalaList->get($currentIndex + 1);
+
+        if ($nextGejala) {
+            $this->currentGejala = $nextGejala;
         } else {
-            // No more symptoms, start diagnosis
+            // tidak ada gejala lagi, mulai diagnosis
             $this->startDiagnosis();
         }
     }
