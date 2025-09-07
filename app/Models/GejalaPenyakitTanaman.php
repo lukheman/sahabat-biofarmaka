b55e9f5d9a3ee5e8b67e701d342377d2e7b174c3
Akmal <akmalthemalong@gmail.com>
2025-09-07 13:48:48 +0800

    feat: model GejalaPenyakitTanaman

---
+++ app/Models/GejalaPenyakitTanaman.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GejalaPenyakitTanaman extends Model
+{
+    protected $table = 'gejala_penyakit_tanaman';
+    protected $guarded = [];
+}
